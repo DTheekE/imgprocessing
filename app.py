982843ef5a9cb9d200f7f1d12dddcd00078366f6
DTheekE <132099428+DTheekE@users.noreply.github.com>
2023-12-07 19:41:39 +0000

    allforus

--- app.py
+++ app.py
@@ -6,15 +6,6 @@ from keras.models import load_model
 import requests
 from bs4 import BeautifulSoup
 
-from hwrd import download_image
-# Example usage
-image_name_to_download = "img.jpg"
-destination_path_to_save = "saved/img.jpg"
-
-if download_image(image_name_to_download, destination_path_to_save):
-    print(f"Image downloaded successfully to {destination_path_to_save}")
-else:
-    print(f"Image {image_name_to_download} does not exist in Firebase Storage.")
 
 
 model = load_model('FV.h5')
